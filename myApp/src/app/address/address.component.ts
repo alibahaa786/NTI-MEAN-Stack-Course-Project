b17752cbbca9096233b8360de65cdbe82221df8c
alibahaa786 <133379087+alibahaa786@users.noreply.github.com>
2024-12-24 16:59:18 +0200

    Initial template-driven form for billing address is complete

--- myApp/src/app/address/address.component.ts
+++ myApp/src/app/address/address.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-address',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './address.component.css'
 })
 export class AddressComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 
+  selectedCountry = 'egypt'
 }
